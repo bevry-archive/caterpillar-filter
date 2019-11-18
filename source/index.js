08e0c16accdec483d835dd177f5e63f7febb5b9d
Benjamin Lupton <b@lupton.cc>
2019-11-18 08:16:31 +0800

    update a docs link

--- source/index.js
+++ source/index.js
@@ -7,7 +7,7 @@ const { Transform } = require('caterpillar')
 
 /**
 Filter the log entires by level.
-Extends http://rawgit.com/bevry/caterpillar/master/docs/index.html#Transform
+Extends http://master.caterpillar.bevry.surge.sh/docs/Transform.html
 @extends Transform
 @example
 const logger = require('caterpillar').create()
